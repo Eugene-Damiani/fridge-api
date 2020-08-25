462cd2892fdfcd51630b62f398ff6420598a741e
Eugene-Damiani <damianieugene@gmail.com>
2020-08-25 08:34:54 -0700

    curl-scripts for items

--- curl-scripts/items/destroy.sh
+++ curl-scripts/items/destroy.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-API="http://localhost:4741"
-URL_PATH="/examples"
-
-curl "${API}${URL_PATH}/${ID}" \
+curl "http://localhost:4741/items/${ID}" \
   --include \
   --request DELETE \
   --header "Authorization: Bearer ${TOKEN}"
