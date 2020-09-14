fef63e5da6a7cc5bb1d8cb262e9ee6f671629b05
Eugene-Damiani <damianieugene@gmail.com>
2020-09-13 21:38:18 -0700

    added business and image route

--- app/routes/upload_routes.js
+++ app/routes/upload_routes.js
@@ -4,22 +4,45 @@ const storage = multer.memoryStorage()
 const upload = multer({ storage })
 const Upload = require('../models/upload')
 const router = express.Router()
-const passport = require('passport')
-const requireToken = passport.authenticate('bearer', { session: false })
+// const passport = require('passport')
+// const requireToken = passport.authenticate('bearer', { session: false })
 
 const s3Upload = require('../../lib/s3_upload')
 
-router.post('/uploads', requireToken, upload.single('upload'), (req, res, next) => {
-  req.body.item.owner = req.user.id
+router.post('/uploads', upload.single('upload'), (req, res, next) => {
+  // req.body.upload.owner = req.user.id
   console.log(req.file)
   s3Upload(req.file)
     .then(awsFile => {
       console.log(awsFile)
-      return Upload.create({ owner: req.user.id, url: awsFile.Location })
+      return Upload.create({ url: awsFile.Location })
     })
     .then(uploadDoc => {
       res.status(201).json({ upload: uploadDoc })
     })
+    .catch(next)
+})
+
+// SHOW
+// GET /s/5a7db6c74d55bc51bdf39793
+router.get('/uploads/:id', (req, res, next) => {
+  // req.params.id will be set based on the `:id` in the route
+  Upload.findById(req.params.id)
+    // if `findById` is succesful, respond with 200 and "" JSON
+    .then(uploads => res.status(200).json({ uploads: uploads.toObject() }))
+    // if an error occurs, pass it to the handler
+    .catch(next)
+})
+
+// Index
+router.get('/uploads/', (req, res, next) => {
+  Upload.find(req.params.id)
+    .then(uploads => {
+      return uploads.map(upload => upload.toObject())
+    })
+    // respond with status 200 and JSON
+    .then(uploads => res.status(200).json({ uploads: uploads }))
+    // if an error occurs, pass it to the handler
     .catch(next)
 })
 
